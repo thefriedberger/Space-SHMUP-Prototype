f10974628e9ce333dba4dfb1bb23d73b85711042
Tyler Friedberg <thefriedberger@gmail.com>
2017-10-06 10:00:49 -0400

    Enemy_0 spawns at random location

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public float speed = 10f;
+    public float fireRate = 0.3f;
+    public float health = 10;
+    public int score = 100;
+
+    public bool ______________________;
+
+    public Bounds bounds;
+    public Vector3 boundsCenterOffset;
+	
+    void Awake () {
+        InvokeRepeating("CheckOffScreen", 0f, 2f);
+    }
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    public virtual void Move() {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+
+    public Vector3 pos {
+        get {
+            return (this.transform.position);
+        }
+        set {
+            this.transform.position = value;
+        }
+    }
+
+    void CheckOffScreen () {
+        if (bounds.size == Vector3.zero) {
+            bounds = Utils.CombineBoundsOfChildren(this.gameObject);
+
+            boundsCenterOffset = bounds.center - transform.position;
+        }
+
+        bounds.center = transform.position + boundsCenterOffset;
+        Vector3 off = Utils.ScreenBoundsCheck(bounds, BoundsTest.offScreen);
+        if (off != Vector3.zero) {
+            if(off.y < 0) {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/Main.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+    static public Main S;
+
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = 0.5f;
+    public float enemySpawnPadding = 1.5f;
+
+    public bool _____________________;
+
+    public float enemySpawnRate;
+
+	// Use this for initialization
+	void Awake () {
+        S = this;
+        Utils.SetCameraBounds(this.GetComponent<Camera>());
+        enemySpawnRate = 1f / enemySpawnPerSecond;
+        Invoke("SpawnEnemy", enemySpawnRate);
+	}
+
+	public void SpawnEnemy() {
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate(prefabEnemies[ndx]) as GameObject;
+        Vector3 pos = Vector3.zero;
+        float xMin = Utils.camBounds.min.x + enemySpawnPadding;
+        float xMax = Utils.camBounds.max.x - enemySpawnPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = Utils.camBounds.max.y + enemySpawnPadding;
+        go.transform.position = pos;
+
+        Invoke("SpawnEnemy", enemySpawnRate);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
